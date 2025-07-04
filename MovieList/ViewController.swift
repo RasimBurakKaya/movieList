9a33a6c623438c2d08108f71e97a0dcac114c6af
RasimBurakKaya <kayarasimburak@gmail.com>
2025-07-04 08:44:16 +0300

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Rasim Burak Kaya on 4.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieListTests/MovieListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListTests.swift
+//  MovieListTests
+//
+//  Created by Rasim Burak Kaya on 4.07.2025.
+//
+
+import Testing
+@testable import MovieList
+
+struct MovieListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
